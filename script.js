e8e7797ebd17b47babbab83acd67d471983bb4fe
NithishkumarD <131968677+NithishkumarD@users.noreply.github.com>
2024-04-10 21:30:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function nav() {
+    var navSmall = document.getElementById("nav-small");
+    if (navSmall.style.display === "block") {
+        navSmall.style.display = "none";
+    } else {
+        navSmall.style.display = "block";
+    }
+}
